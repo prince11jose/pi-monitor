466fedd565c27a8f37ef59249ca7c16d25318edb
Prince Jose <prince11jose@hotmail.com>
2021-09-30 11:53:57 +0000

    reboot

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 sudo apt-get install chromium-browser unclutter lightdm -y
 touch ~/.xsession
-echo"xset s off
+echo "xset s off
 xset -dpms
 xset s noblank
 sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/chromium-browser Default/Preferences
 chromium-browser --noerrdialogs --kiosk ~/pi-monitor/spacedesk-offline/spacedesk.html --incognito --disable-translate --window-size=1024,768 --window-position=0,0" >> ~/.xsession
+sudo reboot
